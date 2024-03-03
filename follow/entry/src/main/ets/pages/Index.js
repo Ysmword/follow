505ac439ff9bbde2731efddf7fa0337a93a4a776
yangshimiao <1843121593@qq.copm>
2024-03-03 18:00:02 +0800

    [add] 添加app相关代码

---
+++ follow/entry/src/main/ets/pages/Index.js
@@ -0,0 +1,21 @@
+"use strict";
+class item {
+    constructor(header, image, content) {
+        this.header = header;
+        this.image = image;
+        this.content = content;
+    }
+}
+struct Index extends   {
+    constructor() { }
+    SubMenu() {
+        ;
+    }
+    ClassMenu() {
+        ;
+    }
+    build() {
+        .width("100%");
+    }
+}
+//# sourceMappingURL=Index.js.map
